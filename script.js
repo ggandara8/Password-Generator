1a7bb1c3d76b4a7ffb90e942240df060fc2d53ae
ggandara8 <ggandara8@gmail.com>
2020-04-11 01:36:34 -0500

    commit

--- script.js
+++ script.js
@@ -1,93 +1,111 @@
-// Assignment Code DOM element
-var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-// function writePassword() {
-//   var password = generatePassword();
-//   var passwordText = document.querySelector("#password");
+var generateButton = document.querySelector("#generate");
 
-//   passwordText.value = password;
-// }
-function generatePassword(upper, lower, Spchar, num, FinalLenght) {
-  var typesCount = upper + lower + Spchar + num;
-  console.log(typesCount);
-}
-
-generateBtn.addEventListener("click", () => {
-  Filters();
-  RandomUcLetter();
-  RandomLcLetter();
-  RandomChar();
-  RandomNum(); 
+generateButton.addEventListener('click', function(event){
+event.preventDefault(),
+writePassword()
 });
 
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
 
-function Filters() {
-  // Do while loop to filter if lenght is under 8 or over 128
-  do {
-    var lenght = prompt("Please indicate password lenght? (No shorter than 8 characters)");
-    if (lenght < 8 || lenght > 128 || isNaN(lenght)){
-      alert("Password has to be at least 8 characters long and no longer than 128 characters. Please try again.");
-    }
-  } while (lenght < 8 || lenght > 128 || isNaN(lenght));
+  passwordText.value = password;
+
+}
+
+var LowerCvalue = "abcdefghijklmnopqrstuvwxyz";
+var UpperCvalue = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var spChar = " !#$&'()*+,-./:;<=>?@[\]^_`{|}~";
+var num = "123456789";
+
+// Prompts that come up after you click generate password
+function generatePassword() {
+    do {
+        var Promptlenght = prompt("Please indicate password lenght? (No shorter than 8 characters)");
+          if (Promptlenght < 8 || Promptlenght > 128 || isNaN(Promptlenght)){
+            alert("Password has to be at least 8 characters long and no longer than 128 characters. Please try again.");
+          }
+      } while (Promptlenght < 8 || Promptlenght > 128 || isNaN(Promptlenght));
+        
+          var Userlenght = parseInt(Promptlenght); 
+      
+          var UserLowC = confirm("Please indicate if you want a lowercase letter?");
     
-    var Userlenght = parseInt(lenght);
-    return Userlenght; 
-}
-
-var RandomFunction = {
-  upper: RandomUcLetter,
-  lower: RandomLcLetter,
-  Spchar: RandomChar,
-  num: RandomNum,
-  FinalLenght: Filters
-};
-
-// Function with random uppercase letters in a string
-function RandomUcLetter() {
-  var UpperCvalue = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  var UsserUpC = confirm("Please indicate if you want a Uppercase letter?");
-  if (UsserUpC) {
-  return UpperCvalue[Math.floor(Math.random() * UpperCvalue.length)];
-  } else {
-    return false
+          var UserUpC = confirm("Please indicate if you want a Uppercase letter?");
+          
+          var UserChar = confirm("Please indicate if you want a special character?");
+          
+          var UserNum = confirm("Please indicate if you want a number?");
+          
+          if (UserLowC === false && UserUpC === false && UserChar === false && UserNum === false){
+            alert("You have to choose at least one option");
+            generatePassword();
+          }
+  var generateCount = "";
+  const ObjectFunc = {
+    number: NM(),
+    lower: LC(),
+    upper: UC(),
+    specialChar: SP()
   }
-}
+  var func = Object.values(ObjectFunc);
+  console.log(func);
+
+   for (let i = 0; i < Userlenght; i += count){
+    return func [Math.floor(Math.random() * func.length)];
+   }
 
 
-// Function with random lowercase letter in a string
-function RandomLcLetter() {
-  var LowerCvalue = "abcdefghijklmnopqrstuvwxyz";
-  var UsserLowC = confirm("Please indicate if you want a lowercase letter?");
-  if (UsserLowC) {
-  return LowerCvalue[Math.floor(Math.random() * LowerCvalue.length)];
+
+  function NM() {
+  if (UserNum === true){
+    return num[Math.floor(Math.random() * num.length)];
   } else {
     return false;
   }
-}
+  }
 
-// Function with random special char in a string
-function RandomChar() {
-  var spChar = " !#$&'()*+,-./:;<=>?@[\]^_`{|}~";
-  var UsserChar = confirm("Please indicate if you want a special character?");
-  if (UsserChar) {
+  function LC() {
+  if (UserLowC === true){
+    return LowerCvalue[Math.floor(Math.random() * LowerCvalue.length)]; 
+  } else {
+    return false;
+  }
+  }
+
+  function UC() {
+  if (UserUpC === true) {
+    return UpperCvalue[Math.floor(Math.random() * UpperCvalue.length)];
+  } else {
+    return false;
+  }
+  }
+
+  function SP() {
+  if (UserChar === true){
     return spChar[Math.floor(Math.random() * spChar.length)];
   } else {
-    return false
+    return false;
   }
-}
-
-// Function with random num in a string
-function RandomNum() {
-  var num = "123456789";
-  var UsserNum = confirm("Please indicate if you want a number?");
-  if (UsserNum) {
-    return num[Math.floor(Math.random() * num.length)];
-  } else {
-    return false
   }
+
+  var count = "";
+  if (UserNum === true) {
+    count++;
+  }
+
+  if (UserLowC === true) {
+    count++;
+  }
+
+  if (UserUpC === true) {
+    count++;
+  }
+
+  if (UserChar === true) {
+    count++;
+  }
+
 }
-
-
-// Add event listener to generate button
-// generateBtn.addEventListener("click", writePassword);
